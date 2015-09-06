7d1830512990a4f68c39e79934a9bf50926f0d09
Yosub Shin <yosub@yosubshin.com>
2015-09-05 23:18:30 -0500

    Added scene for level 1

---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleController : MonoBehaviour {
+	public bool isBallIn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Object entered the trigger");
+		Rigidbody rb = other.GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0, 0, 0);
+		rb.detectCollisions = false;
+	}
+
+	void OnTriggerStay (Collider other) {
+		Debug.Log ("Object is within trigger");
+		float ballX = other.transform.position.x;
+		float ballY = other.transform.position.y;
+		float ballZ = other.transform.position.z;
+		Rigidbody rb = other.GetComponent<Rigidbody> ();
+		if (ballY > -0.1) {
+			rb.MovePosition(new Vector3(ballX, ballY - 0.01f, ballZ));
+		} else {
+			isBallIn = true;
+			rb.velocity = new Vector3 (0, 0, 0);
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		Debug.Log ("Object exited trigger");
+	}
+}
---
+++ Assets/Scripts/InitialBallPush.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitialBallPush : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
