a1f23ff2f0be8071aaf00285d519c090dd66cc96
Se-Joon Chung <sejoon210@gmail.com>
2015-09-06 02:03:10 -0400

    Initial position setting & collider

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController gameController;
+
+	void Awake() {
+		if (gameController == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			gameController = this;
+		}
+		else if (gameController != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		setPlayerPosition();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setPlayerPosition()
+	{
+		GameObject golfBall = GameObject.Find("GolfBall");
+
+		GameObject player = GameObject.Find("Player");
+		Vector3 pos = golfBall.transform.position;
+		pos.x -= 0.05f;
+		pos.y = player.transform.position.y;
+		pos.z -= 0.05f;
+
+		player.transform.position = pos;
+	}
+}
--- Assets/Scripts/PutterController.cs
+++ Assets/Scripts/PutterController.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 namespace UnityStandardAssets.Characters.FirstPerson
 {
 	[RequireComponent(typeof (Rigidbody))]
-	[RequireComponent(typeof (CapsuleCollider))]
 	public class PutterController : MonoBehaviour
 	{
 		[Serializable]
@@ -142,7 +141,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 		private void Start()
 		{
 			m_RigidBody = GetComponent<Rigidbody>();
-			m_Capsule = GetComponent<CapsuleCollider>();
 			mouseLook.Init (transform, cam.transform);
 
 			// Setup so that DLL can call Unity's Debug.Log
@@ -165,12 +163,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 		
 		private void Update()
 		{
-//			RotateView();
-			
-			if (CrossPlatformInputManager.GetButtonDown("Jump") && !m_Jump)
-			{
-				m_Jump = true;
-			}
 		}
 		
 		
@@ -181,56 +173,14 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
 			handleEvent();
 			IntPtr pWiimote6dof = Wiimote6DOF();
-			float[] result = new float[ 3 ];
-			Marshal.Copy( pWiimote6dof, result, 0, 3 );
-			FreeMemory(pWiimote6dof);
+			if (pWiimote6dof != null)
+			{
+				float[] result = new float[ 3 ];
+				Marshal.Copy( pWiimote6dof, result, 0, 3 );
+				FreeMemory(pWiimote6dof);
 
-			transform.parent.localEulerAngles = new Vector3(result[2], 90,  -result[0]-90);
-//			m_IsGrounded = true;
-//			
-//			if ((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && (advancedSettings.airControl || m_IsGrounded))
-//			{
-//				// always move along the camera forward as it is the direction that it being aimed at
-//				Vector3 desiredMove = cam.transform.forward*input.y + cam.transform.right*input.x;
-//				desiredMove = Vector3.ProjectOnPlane(desiredMove, m_GroundContactNormal).normalized;
-//				
-//				desiredMove.x = desiredMove.x*movementSettings.CurrentTargetSpeed;
-//				desiredMove.z = desiredMove.z*movementSettings.CurrentTargetSpeed;
-//				desiredMove.y = desiredMove.y*movementSettings.CurrentTargetSpeed;
-//				if (m_RigidBody.velocity.sqrMagnitude <
-//				    (movementSettings.CurrentTargetSpeed*movementSettings.CurrentTargetSpeed))
-//				{
-//					m_RigidBody.AddForce(desiredMove*SlopeMultiplier(), ForceMode.Impulse);
-//					transform.Translate(desiredMove);
-//				}
-//			}
-//
-//			if (m_IsGrounded)
-//			{
-//				m_RigidBody.drag = 5f;
-//				
-//				if (m_Jump)
-//				{
-//					m_RigidBody.drag = 0f;
-//					m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
-//					m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
-//					m_Jumping = true;
-//				}
-//				
-//				if (!m_Jumping && Mathf.Abs(input.x) < float.Epsilon && Mathf.Abs(input.y) < float.Epsilon && m_RigidBody.velocity.magnitude < 1f)
-//				{
-//					m_RigidBody.Sleep();
-//				}
-//			}
-//			else
-//			{
-//				m_RigidBody.drag = 0f;
-//				if (m_PreviouslyGrounded && !m_Jumping)
-//				{
-//					StickToGroundHelper();
-//				}
-//			}
-//			m_Jump = false;
+				transform.parent.localEulerAngles = new Vector3(result[2], 0,  -result[0]-97);
+			}
 		}
 		
 		
@@ -238,21 +188,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 		{
 			float angle = Vector3.Angle(m_GroundContactNormal, Vector3.up);
 			return movementSettings.SlopeCurveModifier.Evaluate(angle);
-		}
-		
-		
-		private void StickToGroundHelper()
-		{
-			RaycastHit hitInfo;
-			if (Physics.SphereCast(transform.position, m_Capsule.radius, Vector3.down, out hitInfo,
-			                       ((m_Capsule.height/2f) - m_Capsule.radius) +
-			                       advancedSettings.stickToGroundHelperDistance))
-			{
-				if (Mathf.Abs(Vector3.Angle(hitInfo.normal, Vector3.up)) < 85f)
-				{
-					m_RigidBody.velocity = Vector3.ProjectOnPlane(m_RigidBody.velocity, hitInfo.normal);
-				}
-			}
 		}
 		
 		
@@ -286,28 +221,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 				m_RigidBody.velocity = velRotation*m_RigidBody.velocity;
 			}
 		}
-		
-		
-		/// sphere cast down just beyond the bottom of the capsule to see if the capsule is colliding round the bottom
-		private void GroundCheck()
-		{
-			m_PreviouslyGrounded = m_IsGrounded;
-			RaycastHit hitInfo;
-			if (Physics.SphereCast(transform.position + m_Capsule.center, m_Capsule.radius, Vector3.down, out hitInfo,
-			                       ((m_Capsule.height/2f) - m_Capsule.radius) + advancedSettings.groundCheckDistance))
-			{
-				m_IsGrounded = true;
-				m_GroundContactNormal = hitInfo.normal;
-			}
-			else
-			{
-				m_IsGrounded = false;
-				m_GroundContactNormal = Vector3.up;
-			}
-			if (!m_PreviouslyGrounded && m_IsGrounded && m_Jumping)
-			{
-				m_Jumping = false;
-			}
-		}
+
 	}
 }
