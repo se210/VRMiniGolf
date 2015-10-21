a1c221339a626d0cee812c9e248f9384a5ecc224
Se-Joon Chung <sejoon210@gmail.com>
2015-10-21 14:41:50 -0400

    Golf ball stops if below threshold

---
+++ Assets/Scripts/GolfBallBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolfBallBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+		if (rb.velocity.magnitude < 0.1)
+		{
+			rb.velocity = new Vector3(0,0,0);
+			rb.angularVelocity = new Vector3(0,0,0);
+		}
+	}
+}
